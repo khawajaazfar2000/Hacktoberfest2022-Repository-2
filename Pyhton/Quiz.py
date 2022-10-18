cedf5fdbf6f1f5322b3e293b0afff22a83422157
shiva1217 <107601133+shiva1217@users.noreply.github.com>
2022-10-18 19:02:52 +0530

    Update Quiz.py

--- Pyhton/Quiz.py
+++ Pyhton/Quiz.py
@@ -1,30 +1,30 @@
-print('Welcome to Maths Quiz')
+print('Welcome to GK Quiz')
 answer=input('Are you ready to play the Quiz ? (yes/no) :')
 score=0
 total_questions=3
 
 if answer.lower()=='yes':
-    answer=input('Question 1:25+36')
-    if answer.lower()=='61':
+    answer=input('Question 1: Dead Sea is located between which two countries?')
+    if answer.lower()=='Jordan and Israel':
         score += 1
         print('correct')
     else:
-        print('Wrong Answer')
+        print('Wrong Answer :(')
 
 
-    answer=input('Question 2: 64*62')
-    if answer.lower()=='3968':
+    answer=input('Question 2: Which country is known as Country of Rising Sun?')
+    if answer.lower()=='Japan':
         score += 1
         print('correct')
     else:
-        print('Wrong Answer')
+        print('Wrong Answer :(')
 
-    answer=input('Question 3: 64-3')
-    if answer.lower()=='61':
+    answer=input('Question 3: White Elephant are found in .....')
+    if answer.lower()=='Thailand':
         score += 1
         print('correct')
     else:
-        print('Wrong Answer')
+        print('Wrong Answer :(')
 
 print('Well Done!, you attempted',score,"questions correctly!")
 mark=(score/total_questions)*100
