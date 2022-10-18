cca21480ec5a12f81c023ca221bcf372db01b2fe
shiva1217 <107601133+shiva1217@users.noreply.github.com>
2022-10-18 14:00:44 +0530

    Create Quiz.py

---
+++ Pyhton/Quiz.py
@@ -0,0 +1,31 @@
+print('Welcome to GK Quiz')
+answer=input('Are you ready to play the Quiz ? (yes/no) :')
+score=0
+total_questions=3
+
+if answer.lower()=='yes':
+    answer=input('Question 1: Dead Sea is located between which two countries?')
+    if answer.lower()=='Jordan and Israel':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+
+
+    answer=input('Question 2: Which country is known as 'Country of Rising Sun'? ')
+    if answer.lower()=='Japan':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+
+    answer=input('Question 3: White Elephant are found in .....')
+    if answer.lower()=='Thailand':
+        score += 1
+        print('correct')
+    else:
+        print('Wrong Answer :(')
+
+print('Well Done!, you attempted',score,"questions correctly!")
+mark=(score/total_questions)*100
+print('Marks obtained:',mark)
